f2c0bd36301e0b62b96e4aa986c790f74975f3e3
kalev <Evgeniy.Kalmykov@technopark.ru>
2022-12-22 12:52:06 +0300

    доработки тестов. Добавление матчеров из js-dom

--- src/components/Button/test/index.spec.tsx
+++ src/components/Button/test/index.spec.tsx
@@ -1,26 +1,26 @@
 import { describe, expect, it, vi } from 'vitest'
 import { fireEvent, render } from '@testing-library/react'
 
-import { UiButton } from '../index'
+import { InfButton } from '../index'
 import '@testing-library/jest-dom/extend-expect'
 import { TestSelectors } from '../../../../test/selectors'
 
 describe('hello world', () => {
   it('matchSnapshot', () => {
-    const reactEl = render(<UiButton>Button</UiButton>)
+    const reactEl = render(<InfButton>Button</InfButton>)
     expect(reactEl).toMatchSnapshot()
   })
 
   it('default variant', () => {
-    const reactEl = render(<UiButton />)
+    const reactEl = render(<InfButton />)
 
     const el = reactEl.queryByTestId(TestSelectors.button.root)
-    expect(el?.className).contains('ui-button--variant-red')
+    expect(el?.className).contains('inf-button--variant-primary')
   })
 
   it('click', () => {
     const clickFn = vi.fn()
-    const { container } = render(<UiButton onClick={clickFn}>Нажми</UiButton>)
+    const { container } = render(<InfButton onClick={clickFn}>Нажми</InfButton>)
     const el = container.firstChild as HTMLElement
     fireEvent.click(el)
     expect(clickFn).toHaveBeenCalledOnce()
