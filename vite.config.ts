072355cac53b311711c00d543fc3e24cf0409afe
kalev <Evgeniy.Kalmykov@technopark.ru>
2023-02-22 11:16:02 +0300

    Сменил местами энтрипоинты в билде из-за ошибки с индексфайлом

--- vite.config.ts
+++ vite.config.ts
@@ -34,8 +34,8 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: [
-        resolve(__dirname, 'src/styles/global.scss'),
-        resolve(__dirname, 'src/index.ts')
+        resolve(__dirname, 'src/index.ts'),
+        resolve(__dirname, 'src/styles/global.scss')
       ],
       name: 'ui-components',
       formats: ['es', 'cjs'],
