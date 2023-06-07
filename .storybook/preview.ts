7da893a5670f375cf4bcbaf992abace7ecbd128e
vinderman <vinderman771@gmail.com>
2023-06-07 08:22:59 +0000

    Откат до версии 7.0.6

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,45 +1,42 @@
 import '../src/styles/global.scss'
-import { Preview } from '@storybook/react'
 
-export const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  },
+
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'clean',
+        value: '#fff'
+      },
+      {
+        name: 'light',
+        value: '#f2f4f5'
+      },
+      {
+        name: 'dark',
+        value: '#333333'
       }
-    },
-
-    backgrounds: {
-      default: 'light',
-      values: [
-        {
-          name: 'clean',
-          value: '#fff'
-        },
-        {
-          name: 'light',
-          value: '#f2f4f5'
-        },
-        {
-          name: 'dark',
-          value: '#333333'
-        }
+    ]
+  },
+  options: {
+    storySort: {
+      order: [
+        'Intro',
+        'Components',
+        'Typography',
+        'Form',
+        'Overlay',
+        'Layout',
+        'Demo'
       ]
-    },
-    options: {
-      storySort: {
-        order: [
-          'Intro',
-          'Components',
-          'Typography',
-          'Form',
-          'Overlay',
-          'Layout',
-          'Demo'
-        ]
-      }
     }
   }
 }
