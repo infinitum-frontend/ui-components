32497eadb0869b4d40584f0c04612eb2fdec344b
Rusaldo <yandiev_97@mail.ru>
2024-01-18 12:02:51 +0300

    Card / расширение API (#32)
    * Card / расширение API

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { ElementType, forwardRef, ReactElement } from 'react'
-import { PolymorphicComponent, PolymorphicRef } from '~/src/utils/types'
+import {
+  PolymorphicComponent,
+  PolymorphicRef,
+  SpaceVariants
+} from '~/src/utils/types'
 import cn from 'classnames'
 import './Card.scss'
+import { Box, BoxProps } from '../Box'
+
+type Size = 'medium' | 'large'
 
 export interface CardProps {
-  size?: 'medium' | 'large'
+  size?: Size
+  variant?: 'shadow' | 'outline'
   outlineVariant?: 'danger'
+  borderRadius?: BoxProps['borderRadius']
 }
 
 function BaseCard<C extends ElementType = 'div'>(
@@ -18,54 +27,45 @@ function BaseCard<C extends ElementType = 'div'>(
     className,
     as = 'div',
     size = 'medium',
+    variant = 'outline',
     outlineVariant,
+    borderRadius,
     ...rest
   } = props
 
-  const Component = as
+  const sizeToPadding: Record<Size, SpaceVariants> = {
+    medium: 'medium',
+    large: 'large'
+  }
 
+  let borderWidth: BoxProps['borderWidth']
+  let borderColor: BoxProps['borderColor']
+
+  if (variant === 'outline') {
+    borderWidth = 'default'
+    borderColor = 'default'
+  }
+
+  if (outlineVariant === 'danger') {
+    borderWidth = 'thick'
+    borderColor = 'danger'
+  }
   return (
-    <Component
+    <Box
       ref={ref}
-      className={cn('inf-card', className, `inf-card--size-${size as string}`, {
-        [`inf-card--outline-${outlineVariant as string}`]: outlineVariant
-      })}
+      className={cn('inf-card', className)}
+      as={as}
+      boxShadow={variant === 'outline' ? undefined : 'medium'}
+      borderRadius={borderRadius}
+      background="default"
+      padding={sizeToPadding[size]}
+      borderWidth={borderWidth}
+      borderColor={borderColor}
       {...rest}
     >
       {children}
-    </Component>
+    </Box>
   )
-
-  // TODO: Возможно в будущем стоит реализовать через Box
-  // const sizeToPadding = {
-  //   medium: 'medium',
-  //   large: 'large'
-  // }
-
-  // let borderProps
-
-  // if (outlineVariant) {
-  //   borderProps = {
-  //     borderWidth: 'thick',
-  //     borderColor: 'danger'
-  //   }
-  // }
-
-  // return (
-  //   <Box
-  //     ref={ref}
-  //     className={cn('inf-card', className)}
-  //     as={as}
-  //     boxShadow="medium"
-  //     borderRadius="small"
-  //     background="default"
-  //     padding={sizeToPadding[size]}
-  //     {...borderProps}
-  //     {...rest}
-  //   >
-  //     {children}
-  //   </Box>
-  // )
 }
 
 const Card = forwardRef(BaseCard)
