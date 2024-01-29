0deee6bc36befb59bd968c1a5f19834be2dfd79b
kalev <Evgeniy.Kalmykov@technopark.ru>
2024-01-29 17:38:10 +0300

    Документирование таблицы

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -42,7 +42,10 @@ export interface TableProps extends TableBaseProps {
   verticalAlignHead?: TableVerticalAlignValue
   /** CSS свойство vertical-align для рядов */
   verticalAlignBody?: TableVerticalAlignValue
-  /** Максимальное количество отображаемых элементов */
+  /**
+   * @deprecated
+   * Максимальное количество отображаемых элементов
+   */
   maxLength?: number
   /** Включение сортировки по столбцам */
   withSorting?: boolean
@@ -51,10 +54,17 @@ export interface TableProps extends TableBaseProps {
   /** Событие изменения состояния сортировки */
   onSortingChange?: OnChangeFn<SortingState>
   // TODO: sorting mode auto
+  /** @deprecated */
   withFiltering?: boolean
-  /** Событие изменения состояния фильтров */
+  /**
+   * @deprecated
+   * Событие изменения состояния фильтров
+   */
   onFiltersChange?: OnChangeFn<TableColumnFiltersState>
-  /** Начальное состояние фильтров */
+  /**
+   * @deprecated
+   * Начальное состояние фильтров
+   */
   filtersState?: TableColumnFiltersState
   /** Отображение чекбоксов в 1 колонке */
   withRowSelection?: boolean
