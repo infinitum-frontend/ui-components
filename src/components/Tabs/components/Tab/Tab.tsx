b42ea715aba2ed3a398d33502be671f9b88a580b
vinderman <42290996+vinderman@users.noreply.github.com>
2024-02-02 12:21:46 +0300

    remove badge prop from Tab (#34)
    Co-authored-by: kalev <Evgeniy.Kalmykov@technopark.ru>

--- src/components/Tabs/components/Tab/Tab.tsx
+++ src/components/Tabs/components/Tab/Tab.tsx
@@ -10,10 +10,8 @@ import cn from 'classnames'
 import useTabsContext from 'Components/Tabs/context/useTabsContext'
 import './Tab.scss'
 import { PolymorphicComponent } from '~/src/utils/types'
-import { Badge } from 'Components/Badge'
 
 export interface TabProps {
-  badge?: string | number
   icon?: ReactNode
   disabled?: boolean
   /** Состояние активности элемента. Для корректной работы необходимо всем табам установить в данный проп булево значение */
@@ -27,7 +25,6 @@ const Tab = <C extends ElementType = 'button'>({
   type,
   disabled = false,
   active: activeProp,
-  badge,
   icon,
   ...props
 }: PolymorphicComponent<C, TabProps>): ReactElement => {
@@ -63,13 +60,6 @@ const Tab = <C extends ElementType = 'button'>({
     >
       {icon && <span className="inf-tab__icon">{icon}</span>}
       {children}
-      {badge && (
-        <Badge
-          count={badge}
-          onClick={() => handleTabClick(id)}
-          className={'inf-tab__badge'}
-        />
-      )}
       {active && <span className={'inf-tab__underline'} />}
     </Component>
   )
