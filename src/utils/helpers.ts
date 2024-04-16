a0cd7bb99baba5250a4b96ce2fb5fb3f21204c86
vinderman <42290996+vinderman@users.noreply.github.com>
2024-04-16 11:11:55 +0300

    Фикс проверки checked-условия у Combobox (#70)
    Co-authored-by: kalev <Evgeniy.Kalmykov@technopark.ru>

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -19,3 +19,11 @@ export function pluralize(count: number, variants: string[]): string {
 export function capitalize(value: string): string {
   return value.charAt(0).toUpperCase() + value.slice(1)
 }
+
+export function checkIsValueExists(value?: any): boolean {
+  if (typeof value === 'number') {
+    return Number.isFinite(value)
+  } else {
+    return Boolean(value)
+  }
+}
