692d3150b1139ae579390807080ef2927f3698b3
Nikita Redozubov <prostor130@gmail.com>
2024-04-18 18:06:25 +0700

    тесты breadcrumbs

---
+++ src/components/Breadcrumbs/fixtures/index.ts
@@ -0,0 +1,41 @@
+export const BreadcrumbsBaseItems = [
+  {
+    title: 'Главная',
+    as: 'a',
+    href: 'https://specdep.ru'
+  },
+  {
+    title: 'Прасс Павел',
+    as: 'a',
+    href: 'https://specdep.ru'
+  },
+  {
+    title: 'Хальмеев Александр',
+    as: 'a',
+    href: 'https://specdep.ru'
+  }
+]
+
+export const BreadcrumbsLongItems = [
+  {
+    title: 'Главная',
+    as: 'a',
+    href: 'https://specdep.ru'
+  },
+  {
+    title: 'Прасс Павел',
+    as: 'a',
+    href: 'https://specdep.ru'
+  },
+  {
+    title: 'Хальмеев Александр',
+    as: 'a',
+    href: 'https://specdep.ru'
+  },
+  {
+    title: 'Леонид Хенкин',
+    as: 'a',
+    href: 'https://specdep.ru'
+  },
+  { title: 'Александр Круглов', as: 'a', href: 'https://specdep.ru' }
+]
