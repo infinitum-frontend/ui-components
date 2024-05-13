7e21d340bc398922d92f845e26e191ff26c11333
Nikita <77201620+ExistencePersik@users.noreply.github.com>
2024-05-13 13:18:50 +0700

    Lable | Новые варианты, новые цвета (#73)
    * новые варианты лейблов, новые цвета

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,20 +1,53 @@
 import { ComponentPropsWithoutRef, ReactElement, ReactNode } from 'react'
 import cn from 'classnames'
 import './Label.scss'
+import { ReactComponent as IconWarning } from 'Icons/warning.svg'
+import { ReactComponent as IconInfo } from 'Icons/info-circle.svg'
+import { ReactComponent as IconCheckIn } from 'Icons/checkInCircle2.svg'
 
 export interface LabelProps
   extends Omit<ComponentPropsWithoutRef<'span'>, 'prefix'> {
-  variant: 'success' | 'danger' | 'warning' | 'info' | 'neutral'
+  /** Варианты лейбла
+   *
+   * variant: 'danger' was deprecated
+   */
+  variant:
+    | 'info'
+    | 'error'
+    | 'success'
+    | 'warning'
+    | 'neutral'
+    | 'brand'
+    | 'violet'
+    | 'teal'
+  /** Поддержка стандартной иконки для variant */
+  withIcon?: boolean
   before?: ReactNode
+}
+
+const defaultIcon = {
+  info: <IconInfo />,
+  error: <IconWarning />,
+  success: <IconCheckIn />,
+  warning: <IconWarning />,
+  neutral: <IconWarning />,
+  brand: <IconWarning />,
+  violet: <IconWarning />,
+  teal: <IconWarning />,
+  danger: <IconWarning />
 }
 
 const Label = ({
   children,
   variant = 'neutral',
+  withIcon = false,
   before
 }: LabelProps): ReactElement => {
   return (
     <span className={cn('inf-label', `inf-label--variant-${variant}`)}>
+      {withIcon && (
+        <span className="inf-label__before">{defaultIcon[variant]}</span>
+      )}
       {before && <span className="inf-label__before">{before}</span>}
       {children}
     </span>
