572952504dcac7cd732b26d848b46af232d32227
Nikita R <prostor130@gmail.com>
2024-06-29 12:20:42 +0700

    UploadButton

---
+++ src/components/UploadButton/UploadButton.stories.tsx
@@ -0,0 +1,130 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as React from 'react'
+import { UploadButton } from './index'
+import { Space } from '../Space'
+import { StoryObj, Meta, StoryFn } from '@storybook/react'
+import { ReactComponent as ArrowUpIcon } from 'Icons/chevronup.svg'
+import { ReactComponent as FileIcon } from 'Icons/file.svg'
+
+const ComponentMeta: Meta<typeof UploadButton> = {
+  title: 'Components/UploadButton',
+  component: UploadButton,
+  args: {
+    children: 'Кнопка загрузки'
+  },
+  parameters: {
+    docs: {
+      source: {
+        excludeDecorators: true
+      }
+    }
+  }
+}
+
+export default ComponentMeta
+
+const Template: StoryFn<typeof UploadButton> = ({ ...args }) => (
+  <UploadButton {...args} />
+)
+
+export const Playground = {
+  render: Template
+}
+
+export const Secondary = {
+  render: Template,
+
+  args: {
+    variant: 'secondary'
+  }
+}
+
+export const Tertiary = {
+  render: Template,
+
+  args: {
+    variant: 'tertiary'
+  }
+}
+
+export const Ghost = {
+  render: Template,
+
+  args: {
+    variant: 'ghost'
+  }
+}
+
+export const GhostWithIcon = {
+  render: Template,
+
+  args: {
+    variant: 'ghost',
+    after: <FileIcon />
+  }
+}
+
+export const Loading = {
+  render: Template,
+
+  args: {
+    loading: true
+  }
+}
+
+export const Disabled = {
+  render: Template,
+
+  args: {
+    disabled: true
+  }
+}
+
+export const Icon: StoryObj<typeof UploadButton> = {
+  render: (args) => (
+    <>
+      <UploadButton before={<ArrowUpIcon />} {...args}>
+        Icon Before
+      </UploadButton>
+      <UploadButton after={<ArrowUpIcon />} {...args}>
+        Icon After
+      </UploadButton>
+      <UploadButton before={<ArrowUpIcon />} after={<ArrowUpIcon />} {...args}>
+        Icon Before and After
+      </UploadButton>
+      <UploadButton icon={<ArrowUpIcon />} {...args} />
+    </>
+  ),
+
+  decorators: [
+    (Story) => (
+      <Space gap="small" direction="horizontal">
+        {Story()}
+      </Space>
+    )
+  ]
+}
+
+export const Sizes: StoryObj<typeof UploadButton> = {
+  render: (args) => (
+    <>
+      <UploadButton {...args} size="large">
+        Загрузить файл
+      </UploadButton>
+      <UploadButton {...args} size="medium">
+        Загрузить файл
+      </UploadButton>
+      <UploadButton {...args} size="small">
+        Загрузить файл
+      </UploadButton>
+    </>
+  ),
+
+  decorators: [
+    (Story) => (
+      <Space gap="small" direction="horizontal">
+        {Story()}
+      </Space>
+    )
+  ]
+}
--- src/components/UploadButton/UploadButton.tsx
+++ src/components/UploadButton/UploadButton.tsx
@@ -1,11 +1,66 @@
-import { ReactElement, ChangeEvent, useId } from 'react'
+import { ReactElement, ChangeEvent, useId, ReactNode } from 'react'
 import { Button } from 'Components/Button'
 
 interface UploadButtonProps {
-  onChange: (e: FileList) => void
+  /**
+   * Содержимое
+   */
+  children?: ReactNode
+  /**
+   * Дополнительный className
+   */
+  className?: string
+  /**
+   * Вариант оформления
+   */
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'ghost'
+  // outlined?: boolean TODO: IDD-303
+  // plain?: boolean TODO: IDD-303
+  /**
+   * Размер
+   */
+  size?: 'small' | 'medium' | 'large'
+  /**
+   * Состояние недоступности
+   */
+  disabled?: boolean
+  /**
+   * Состояние загрузки
+   */
+  loading?: boolean
+  /**
+   * Занимает всю ширину контейнера
+   */
+  block?: boolean
+  /**
+   * Иконка (без текста)
+   */
+  icon?: ReactNode
+  /**
+   * Контент слева от текста
+   */
+  before?: ReactNode
+  /**
+   * Контент справа от текста
+   */
+  after?: ReactNode
+  /**
+   * Загрузка нескольких файлов
+   */
   multiple?: boolean
+  onChange: (e: FileList) => void
 }
 const UploadButton = ({
+  children,
+  className,
+  variant,
+  size,
+  disabled,
+  loading,
+  block,
+  icon,
+  before,
+  after,
   onChange,
   multiple
 }: UploadButtonProps): ReactElement => {
@@ -25,10 +80,16 @@ const UploadButton = ({
       <Button
         as="label"
         htmlFor={domId}
-        className="inf-upload-button"
-        variant="tertiary"
+        variant={variant}
+        size={size}
+        disabled={disabled}
+        loading={loading}
+        block={block}
+        icon={icon}
+        before={before}
+        after={after}
       >
-        Прикрепить...
+        {children}
       </Button>
       <input
         type="file"
