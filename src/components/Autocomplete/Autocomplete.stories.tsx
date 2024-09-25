2e1395dfc6eb1452084e152f359fa1160228273a
Nikita R <prostor130@gmail.com>
2024-09-25 18:11:56 +0700

    stories

--- src/components/Autocomplete/Autocomplete.stories.tsx
+++ src/components/Autocomplete/Autocomplete.stories.tsx
@@ -145,3 +145,73 @@ export const Required: StoryObj<typeof Autocomplete> = {
     )
   }
 }
+
+export const RequiredControlledVariant: StoryObj<typeof Autocomplete> = {
+  render: (args) => {
+    const [open, setOpen] = useState(false)
+    const [query, setQuery] = useState('')
+    const [filteredItems, setFilteredItems] = useState(AutocompleteBaseOptions)
+    const [selectedItem, setSelectedItem] =
+      useState<IAutocompleteOption['value']>('')
+
+    const handleClick = (value: IAutocompleteOption['value']): void => {
+      setSelectedItem(value)
+      setOpen(false)
+    }
+
+    const handleInput = (value: string): void => {
+      setQuery(value)
+      setFilteredItems(
+        AutocompleteBaseOptions.filter((item) => {
+          return item.label.toLowerCase().match(value.toLowerCase())
+        })
+      )
+    }
+
+    const handleInputSubmit = (): void => {
+      setSelectedItem(filteredItems[0].value)
+      setQuery('')
+      setOpen(false)
+    }
+    const handleSubmit = (): void => {
+      alert('submit')
+    }
+
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group required>
+          <Form.Label>Поле</Form.Label>
+          <Autocomplete opened={open} onOpenChange={(value) => setOpen(value)}>
+            <Autocomplete.Button placeholder={'Выберите услугу'}>
+              {
+                AutocompleteBaseOptions.find(
+                  (option) => option.value === selectedItem
+                )?.label
+              }
+            </Autocomplete.Button>
+            <Autocomplete.Dropdown>
+              <Autocomplete.Input
+                onSubmit={handleInputSubmit}
+                value={query}
+                onInput={handleInput}
+                allowClear={true}
+              />
+              <Autocomplete.Options>
+                {filteredItems.map((option) => (
+                  <Autocomplete.Option
+                    onClick={handleClick}
+                    key={option.value}
+                    value={option.value}
+                  >
+                    {option.label}
+                  </Autocomplete.Option>
+                ))}
+              </Autocomplete.Options>
+            </Autocomplete.Dropdown>
+          </Autocomplete>
+        </Form.Group>
+        <Button type="submit">Отправить</Button>
+      </Form>
+    )
+  }
+}
