08c5e526dd846850de82306ddbb51ebfa7d1e2d2
Ruslan Yandiev <yandiev_97@mail.ru>
2024-11-08 14:11:24 +0300

    fix стилей сторибука

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -4,11 +4,15 @@ import logo from '../src/assets/images/logo.svg'
 
 export default create({
   base: 'light',
+
   brandTitle: 'Infinitum UI',
   brandUrl: 'https://specdep.ru/',
   brandImage: logo,
-  brandTarget: '_blank'
-  // colorPrimary: '#f53a3a',
-  // barTextColor: '#c8cacc',
-  // barSelectedColor: '#f53a3a'
+  brandTarget: '_blank',
+
+  colorSecondary: '#f53a3a',
+
+  barTextColor: '#c8cacc',
+  barHoverColor: '#f53a3a',
+  barSelectedColor: '#f53a3a'
 })
