1d318d0cd2ef274b7e708305aa8645c00df9e3d2
Rusaldo <yandiev_97@mail.ru>
2025-01-15 15:15:45 +0300

    IDD-563 Table / добавлена опция visibleOnRowHover для отображения содержимого ячейки только при ховере на ряд (#132)
    IDD-563 Table / добавлена опция visibleOnRowHover для отображения содержимого ячейки только при ховере на ряд

--- src/components/Table/components/TableCell/TableCell.tsx
+++ src/components/Table/components/TableCell/TableCell.tsx
@@ -1,14 +1,23 @@
 import { ComponentPropsWithoutRef, ReactElement } from 'react'
+import cn from 'classnames'
 import './TableCell.scss'
 
-export interface TableCellProps extends ComponentPropsWithoutRef<'td'> {}
+export interface TableCellProps extends ComponentPropsWithoutRef<'td'> {
+  visibleOnHover?: boolean
+}
 
 const TableCell = ({
   children,
+  visibleOnHover,
   ...restProps
 }: TableCellProps): ReactElement => {
   return (
-    <td className="inf-table-cell" {...restProps}>
+    <td
+      className={cn('inf-table-cell', {
+        'inf-table-cell--visible-on-hover': visibleOnHover
+      })}
+      {...restProps}
+    >
       {children}
     </td>
   )
