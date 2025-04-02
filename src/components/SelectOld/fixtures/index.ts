daa47ea8ce73c361923051ec6b224ad19da56060
Ruslan Yandiev <yandiev_97@mail.ru>
2025-04-02 21:39:47 +0300

    Revert "remove old"
    This reverts commit 7d738e651ae93649b68d772cee90e4c481c339bb.

---
+++ src/components/SelectOld/fixtures/index.ts
@@ -0,0 +1,52 @@
+export const SelectBaseOptions = [
+  { label: 'Депозитарные услуги', value: 0 },
+  { label: 'Спецдепозитарные услуги', value: 1 },
+  { label: 'Консалтинг и аутсорсинг', value: 2 }
+]
+
+export const SelectOptionsUnformatted = [
+  { text: 'Депозитарные услуги', id: 0 },
+  { text: 'Спецдепозитарные услуги', id: 1 },
+  { text: 'Консалтинг и аутсорсинг', id: 2 }
+]
+
+export const SelectOptionsWithExtraKeys = [
+  { text: 'Депозитарные услуги', id: 0, subtext: 'Subtext' },
+  { text: 'Спецдепозитарные услуги', id: 1, subtext: 'Subtext' },
+  { text: 'Консалтинг и аутсорсинг', id: 2, subtext: 'Subtext' }
+]
+
+export const SelectOptionsRawLong = [
+  {
+    id: '1',
+    name: 'Ошибка доставки'
+  },
+  {
+    id: '2',
+    name: 'На отправке'
+  },
+  {
+    id: '3',
+    name: 'Доставлен'
+  },
+  {
+    id: '4',
+    name: 'Получен'
+  },
+  {
+    id: '5',
+    name: 'Ошибка в обработке'
+  },
+  {
+    id: '6',
+    name: 'Отправлен'
+  },
+  {
+    id: '7',
+    name: 'Отказано в обработке'
+  },
+  {
+    id: '8',
+    name: 'Сохранено'
+  }
+]
