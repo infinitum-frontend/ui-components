7d738e651ae93649b68d772cee90e4c481c339bb
Ruslan Yandiev <yandiev_97@mail.ru>
2025-04-02 21:33:53 +0300

    remove old

--- src/components/SelectOld/index.ts
+++
@@ -1,6 +0,0 @@
-import Select, { defaultSelectItem } from './Select'
-import selectDataFormatter from './helpers/formatter'
-import { SelectDataFormatterOptions, SelectOption, SelectProps } from './types'
-
-export { defaultSelectItem, Select, selectDataFormatter }
-export type { SelectDataFormatterOptions, SelectOption, SelectProps }
