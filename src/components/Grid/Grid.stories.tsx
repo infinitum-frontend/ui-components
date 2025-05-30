a22b870fb159c92ffc9a5fd5a0dde5bb02c2f9c0
Redozubov <prostor130@gmail.com>
2025-05-30 17:06:55 +0700

    Grid areas, GridItem

--- src/components/Grid/Grid.stories.tsx
+++ src/components/Grid/Grid.stories.tsx
@@ -1,8 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as React from 'react'
-import { Grid } from './index'
-import { Meta, StoryFn } from '@storybook/react'
+import { Grid } from './Grid/index'
+import { Meta, StoryFn, StoryObj } from '@storybook/react'
 import { Box } from 'Components/Box'
+import { GridItem } from './GridItem'
 
 const ComponentMeta: Meta<typeof Grid> = {
   title: 'Layout/Grid',
@@ -36,4 +37,166 @@ const Template: StoryFn<typeof Grid> = ({ ...args }) => (
 
 export const Playground = {
   render: Template
+}
+
+export const WithTemplateAreas: StoryObj<typeof Grid> = {
+  render: (args) => {
+    return (
+      <Grid
+        templateAreas={`
+          "header  header"
+          "sidebar content"
+          "footer  footer"
+        `}
+        templateColumns="1fr 3fr"
+        templateRows="80px 1fr 60px"
+        gap="small"
+        style={{
+          width: '100%',
+          height: '400px'
+        }}
+      >
+        <GridItem
+          area="header"
+          style={{
+            backgroundColor: 'teal',
+            color: 'white'
+          }}
+        >
+          Шапка (строка 1)
+        </GridItem>
+
+        <GridItem
+          area="sidebar"
+          style={{
+            backgroundColor: 'orange'
+          }}
+        >
+          Сайдбар (строка 2)
+        </GridItem>
+
+        <GridItem
+          area="content"
+          style={{
+            backgroundColor: 'blue',
+            color: 'white'
+          }}
+        >
+          Контент (строка 2)
+        </GridItem>
+
+        <GridItem
+          area="footer"
+          style={{
+            backgroundColor: 'purple',
+            color: 'white'
+          }}
+        >
+          Подвал (строка 3)
+        </GridItem>
+      </Grid>
+    )
+  }
+}
+
+export const WithGridItemsPlacement: StoryObj<typeof Grid> = {
+  render: (args) => {
+    return (
+      <Grid
+        templateColumns="repeat(4, 1fr)"
+        templateRows="repeat(3, 100px)"
+        gap="small"
+      >
+        <GridItem
+          placement={{
+            columnStart: 2,
+            columnEnd: 4,
+            rowStart: 1,
+            rowEnd: 3
+          }}
+          style={{
+            backgroundColor: 'teal',
+            color: 'white'
+          }}
+        >
+          Элемент с явным позиционированием Блок 1 (колонка 2-4 × строка 1-3)
+        </GridItem>
+
+        <GridItem
+          placement={{
+            columnStart: 1,
+            rowStart: 2,
+            rowEnd: 'span 2'
+          }}
+          style={{
+            backgroundColor: 'orange'
+          }}
+        >
+          Элемент с комбинированным позиционированием Блок 2 (авто ширина × 2
+          строки)
+        </GridItem>
+
+        <GridItem
+          placement={{
+            columnStart: 'span 2',
+            rowStart: 3
+          }}
+          style={{
+            backgroundColor: 'purple',
+            color: 'white'
+          }}
+        >
+          Элемент с относительным позиционированием Блок 3 (2 колонки × 1
+          строка)
+        </GridItem>
+      </Grid>
+    )
+  }
+}
+
+export const WithGridItemsAlignment: StoryObj<typeof Grid> = {
+  render: (args) => {
+    return (
+      <Grid
+        templateColumns="repeat(3, 150px)"
+        templateRows="repeat(3, 100px)"
+        gap="small"
+      >
+        <GridItem
+          style={{ backgroundColor: 'teal' }}
+          alignSelf="center"
+          justifySelf="center"
+        >
+          Центр
+        </GridItem>
+
+        <GridItem
+          style={{ backgroundColor: 'orange' }}
+          justifySelf="end"
+          alignSelf="end"
+        >
+          Правый низ
+        </GridItem>
+
+        <GridItem
+          style={{ backgroundColor: 'purple', color: 'white' }}
+          justifySelf="stretch"
+        >
+          Растянут по ширине
+        </GridItem>
+
+        <GridItem
+          style={{ backgroundColor: 'red', color: 'white' }}
+          justifySelf="start"
+          alignSelf="start"
+        >
+          Левый верх
+        </GridItem>
+
+        <GridItem style={{ backgroundColor: 'crimson', color: 'white' }}>
+          Авто, по умолчанию
+        </GridItem>
+      </Grid>
+    )
+  }
 }
---
+++ src/components/Grid/GridItem/GridItem.tsx
@@ -0,0 +1,105 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { CSSProperties, ReactElement, ReactNode } from 'react'
+import cn from 'classnames'
+import './GridItem.scss'
+
+export interface GridItemProps {
+  /**
+   * Количество колонок, которые занимает элемент (число или 'auto')
+   */
+  colSpan?: number | 'auto'
+  /**
+   * Количество строк, которые занимает элемент (число или 'auto')
+   */
+  rowSpan?: number | 'auto'
+  /**
+   * Имя области в grid-template-areas, не используем если есть placement
+   */
+  area?: string
+  /**
+   * CSS свойство align-self
+   */
+  alignSelf?: CSSProperties['alignSelf']
+  /**
+   * CSS свойство justify-self
+   */
+  justifySelf?: CSSProperties['justifySelf']
+  /**
+   * Позиционирование элемента, не используем если есть area
+   */
+  placement?: {
+    columnStart?: number | string
+    columnEnd?: number | string
+    rowStart?: number | string
+    rowEnd?: number | string
+  }
+  /**
+   * Дочерние элементы
+   */
+  children?: ReactNode
+  /**
+   * Кастомные классы
+   */
+  className?: string
+  /**
+   * Кастомные стили
+   */
+  style?: CSSProperties
+}
+
+const GridItem = ({
+  colSpan,
+  rowSpan,
+  area,
+  alignSelf,
+  justifySelf,
+  placement,
+  children,
+  className,
+  style,
+  ...rest
+}: GridItemProps): ReactElement => {
+  // исключаем стили placement если есть area, потому что браузер видит их конфликтующими
+  const getGridPositioningStyles = (): CSSProperties => {
+    if (area) {
+      return { gridArea: area }
+    }
+
+    return {
+      gridColumnStart: placement?.columnStart ?? (colSpan ? 'auto' : undefined),
+      gridColumnEnd:
+        placement?.columnEnd ?? (colSpan ? `span ${colSpan}` : undefined),
+      gridRowStart: placement?.rowStart ?? (rowSpan ? 'auto' : undefined),
+      gridRowEnd: placement?.rowEnd ?? (rowSpan ? `span ${rowSpan}` : undefined)
+    }
+  }
+
+  return (
+    <div
+      className={cn('inf-grid-item', className)}
+      style={{
+        gridColumn:
+          colSpan !== undefined
+            ? colSpan === 'auto'
+              ? 'auto'
+              : `span ${colSpan}`
+            : undefined,
+        gridRow:
+          rowSpan !== undefined
+            ? rowSpan === 'auto'
+              ? 'auto'
+              : `span ${rowSpan}`
+            : undefined,
+        ...getGridPositioningStyles(),
+        alignSelf,
+        justifySelf,
+        ...style
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default GridItem
