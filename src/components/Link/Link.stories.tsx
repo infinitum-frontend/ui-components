3b8aeff088fbaa50306d15b03b11231acf565466
Redozubov <prostor130@gmail.com>
2025-06-11 14:59:38 +0700

    Link по новому макету

--- src/components/Link/Link.stories.tsx
+++ src/components/Link/Link.stories.tsx
@@ -2,6 +2,8 @@
 import * as React from 'react'
 import { Link } from './index'
 import { Meta, StoryFn, StoryObj } from '@storybook/react'
+import { IconAdd01 } from '@infinitum-ui/icons'
+import { Space } from '../Space'
 
 const ComponentMeta: Meta<typeof Link> = {
   title: 'Components/Link',
@@ -41,6 +43,65 @@ export const InText: StoryObj<typeof Link> = {
         saepe delectus cupiditate fugit dolor aspernatur expedita sed itaque,
         totam illo, harum obcaecati. Modi optio iste porro commodi laboriosam!
       </div>
+    )
+  }
+}
+
+export const Long: StoryObj<typeof Link> = {
+  render: (args) => {
+    return (
+      <div style={{ width: '100px' }}>
+        <Link as="a" href="javascript:void(0);" multiline>
+          Lorem ipsum dolor sit amet consectetur , adipisicing elit. Fugiat
+          consectetur quisquam, perspiciatis asperiores iure assumenda! Nihil
+          necessitatibus quo doloremque similique deserunt iure eligendi
+          adipisci! Molestias iure aperiam pariatur voluptatibus, doloremque
+          voluptatum rem provident quidem quis et libero reprehenderit delectus
+          earum adipisci, tempore quod?
+        </Link>
+      </div>
+    )
+  }
+}
+
+export const PrefixSuffix: StoryObj<typeof Link> = {
+  render: (args) => {
+    return (
+      <Space>
+        <Link as="a" prefix>
+          Prefix
+        </Link>
+        <Link as="a" suffix>
+          Suffix
+        </Link>
+        <Link as="a" prefix={<IconAdd01 />}>
+          Custom Prefix
+        </Link>
+        <Link as="a" suffix={<IconAdd01 />}>
+          Custom Suffix
+        </Link>
+        <Link as="a" prefix suffix>
+          Prefix+Suffix
+        </Link>
+      </Space>
+    )
+  }
+}
+
+export const LinkButton: StoryObj<typeof Link> = {
+  render: (args) => {
+    return (
+      <Space>
+        <Link as="button" onClick={() => alert('click')}>
+          Link Button
+        </Link>
+        <Link as="button" type="submit" onClick={() => alert('submit')}>
+          Link Button Submit
+        </Link>
+        <Link as="button" suffix={<IconAdd01 />} onClick={() => alert('click')}>
+          Link Button Custom Icon
+        </Link>
+      </Space>
     )
   }
 }
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,22 +1,49 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, { ElementType, forwardRef, ReactElement } from 'react'
+import React, {
+  ComponentPropsWithoutRef,
+  ElementType,
+  forwardRef,
+  isValidElement,
+  ReactElement,
+  ReactNode
+} from 'react'
 import classNames from 'classnames'
 import './Link.scss'
 import { PolymorphicComponent, PolymorphicRef } from '~/src/utils/types'
+import { IconArrowUpRight01Sharp } from '@infinitum-ui/icons'
+import { Icon } from '../Icon'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface LinkProps {
+export interface LinkProps
+  extends Omit<
+    ComponentPropsWithoutRef<'button'>,
+    'multiline' | 'prefix' | 'suffix'
+  > {
   /** Поддержка многострочной ссылки */
   multiline?: boolean
-  // underline: 'always' | 'hover' | 'none'
-  // weight?: 'normal' | 'bold'
+  /**
+   * Контент слева от текста
+   */
+  prefix?: boolean | ReactNode
+  /**
+   * Контент справа от текста
+   */
+  suffix?: boolean | ReactNode
 }
 
 function BaseLink<C extends ElementType = 'a'>(
   props: PolymorphicComponent<C, LinkProps>,
   ref: PolymorphicRef<C>
 ): ReactElement {
-  const { as = 'a', children, className, multiline, ...rest } = props
+  const {
+    as = 'a',
+    children,
+    className,
+    multiline,
+    prefix,
+    suffix,
+    ...rest
+  } = props
 
   const getClassNames: () => string = () => {
     return classNames('inf-link', className, {
@@ -26,9 +53,31 @@ function BaseLink<C extends ElementType = 'a'>(
 
   const Component = as
 
+  const getDefaultIcon = (value: 'prefix' | 'suffix'): ReactNode => {
+    const defaultIcon = <IconArrowUpRight01Sharp />
+
+    const iconPrefix = isValidElement(prefix) ? prefix : defaultIcon
+    const iconSuffix = isValidElement(suffix) ? suffix : defaultIcon
+
+    return (
+      <span
+        className={classNames('inf-link__default-icon', {
+          'inf-link--prefix': value === 'prefix',
+          'inf-link--suffix': value === 'suffix'
+        })}
+      >
+        <Icon size="medium">
+          {value === 'prefix' ? iconPrefix : iconSuffix}
+        </Icon>
+      </span>
+    )
+  }
+
   return (
     <Component ref={ref} className={getClassNames()} {...rest}>
+      {prefix && getDefaultIcon('prefix')}
       {children}
+      {suffix && getDefaultIcon('suffix')}
     </Component>
   )
 }
