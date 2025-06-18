fd200e17df71cfd4349686084303c7c7b0614bf0
kalev <vinderman771@gmail.com>
2025-06-18 11:37:42 +0300

    Тесты для сортировки в таблице

---
+++ src/components/Table/constants/testAttributes.ts
@@ -0,0 +1,6 @@
+export const TableTestAttributes = {
+  headerSortButton: 'table-header-sort-button',
+  sortIsNotSelectedIcon: 'table-sort-is-not-selected-icon',
+  sortIsDescIcon: 'table-sort-is-desc-icon',
+  sortIsAscIcon: 'table-sort-is-asc-icon'
+}
